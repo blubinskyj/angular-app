531268595ecb871947b00a866d36a560cf8fb839
blubinskyj <blubinskyj@gmail.com>
2022-03-23 13:37:37 +0200

    Handled auth controller

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://blubinskyj:bogdan24@cluster0.vjx1p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://blubinskyj:bogdan24@cluster0.vjx1p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,53 @@
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys')
+
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id,
+            }, keys.jwt, {expiresIn: 3600});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: 'Wrong password'
+            })
         }
-    })
+    } else {
+        res.status(404).json({
+            message: 'User with this email not found'
+        })
+    }
 }
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        login: 'register'
-    })
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        res.status(409).json({
+            message: 'This email already exist'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
+
+        }
+    }
 }
