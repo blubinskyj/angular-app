4d64ff333d9b68fed925ba96b8e27a7db1ad0daa
blubinskyj <blubinskyj@gmail.com>
2022-03-23 11:57:42 +0200

    Add models

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
-
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extends: true}));
+app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,13 @@
-module.exports.login = function (req,res){
+module.exports.login = function (req, res) {
     res.status(200).json({
-        login: 'login from controller'
+        login: {
+            email: req.body.email,
+            password: req.body.password
+        }
     })
 }
 
-module.exports.register = function (req,res){
+module.exports.register = function (req, res) {
     res.status(200).json({
         login: 'register'
     })
